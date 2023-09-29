c4d0adef656d3b33d9c2aabe652b781176d2de00
daritos <nekttuman@gmail.com>
2023-09-29 21:36:30 +0300

    separate roomsManger logic from server class

--- Server/Server/Server.h
+++ Server/Server/Server.h
@@ -5,7 +5,8 @@
 #include <QTcpServer>
 #include <QTcpSocket>
 
-#include "Room.h"
+#include "RoomsManager.h"
+
 
 enum clientRequestType {
     createRoom,
@@ -17,29 +18,11 @@ enum clientRequestType {
     getOpponentNick
 };
 
-enum serverResponseType {
-    roomNameCheckFailed,
-    roomNameCheckPassed,
-    roomCreated,
-    roomCreationErr,
 
-    JoiningErrNoRoom,
-    JoiningErrWrongPswd,
-    JoiningErrRoomFull,
-    JoinedToRoom,
-    OpponentNick,
 
-};
 class Server : public QTcpServer {
 Q_OBJECT
-// TODO: create roomManager, in server create func
-//  sendResponce(serverResponseType rt, QList<QString> responseParams)
-//  and send everything throw it
-//  getRequest(clientRequestType rt, QList<QString> requestParams) analogically
-//  and have one instance of roomManager in server
 
-
-    int prevId = 0;
 public:
     Server();
 
@@ -47,29 +30,24 @@ public:
 
     QTcpSocket *socket;
 
-private:
-    QVector<QTcpSocket *> m_sockets;
-    quint16 m_nextBlockSize = 0;
-    QByteArray m_data;
-
 
     using roomId = int;
 
-    QMap<roomId, Room*> M_rooms;
+private:
+    QMap <qintptr,QTcpSocket*> m_sockets;
+    quint16 m_nextBlockSize = 0;
+    QByteArray m_data;
 
-    void createRoom(QString roomName, QString pswd, QString hostNick, QTcpSocket *sender);
-    void sendNick(const QString& nick, QTcpSocket * receiver);
+    RoomsManager *roomsManager;
 
-    void checkRoomNameUniq(QString name, QTcpSocket *sender);
-    bool isUniq(const QString& roomName) ;
-    void tryJoinToRoom(const QString &roomName, QString roomPasswd, QString nick, QTcpSocket *receiver);
-    roomId getRoomId(const QString& roomName);
 
 public slots:
 
     void incomingConnection(qintptr socketDescriptor) override;
 
     void slotReadyRead();
+    void sendResponce_slot(qintptr socketDescriptor, serverResponseType rt, QList<QString> responseParams);
+
 
 private slots:
 
