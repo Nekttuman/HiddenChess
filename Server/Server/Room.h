4458d6f1773e562c7d4080365f8e09767673a547
daritos <nekttuman@gmail.com>
2023-09-30 11:08:34 +0300

    refactoring

--- Server/Server/Room.h
+++ Server/Server/Room.h
@@ -16,40 +16,20 @@ class Room {
     QString m_pswd;
 
 public:
-    Room(qintptr hostSocketDescriptor, QString roomName, QString roomPswd) : m_name(std::move(roomName)),
-                                                                             m_pswd(std::move(roomPswd)) {
-        m_hostSocketDescriptor = hostSocketDescriptor;
-    }
+    Room(qintptr hostSocketDescriptor, QString roomName, QString roomPswd);
 
-    void setHostNick(QString nick) {
-        m_hostNick = nick;
-    }
+    void setHostNick(QString nick) { m_hostNick = nick; }
+
+    void addOpponent(qintptr opponentSocketDescriptor, QString nick);
 
     QString getHostNick() { return m_hostNick; }
 
-    [[nodiscard]] qintptr getHostSocketDescriptor() const {
-        return m_hostSocketDescriptor;
-    }
-
-    void setOpponentNick(QString nick) {
-        m_opponentNick = std::move(nick);
-    }
+    [[nodiscard]] qintptr getHostSocketDescriptor() const { return m_hostSocketDescriptor; }
 
     QString getName() { return m_name; }
 
     bool checkPswd(const QString &pswd) { return m_pswd == pswd; }
 
     bool hasOpponent() const { return m_oponentSocketDescriptor != -1; }
-
-    void addOpponent(qintptr opponentSocketDescriptor, QString nick) {
-        m_oponentSocketDescriptor = opponentSocketDescriptor;
-        m_opponentNick = std::move(nick);
-    }
-
-    QString getOpponentNick(qintptr requesterSocketDescriptor) {
-        if (requesterSocketDescriptor == m_hostSocketDescriptor)
-            return m_opponentNick;
-        return m_hostNick;
-    }
 
 };
