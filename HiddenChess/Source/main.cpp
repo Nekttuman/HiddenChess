7c89dda80a4a4ab7b485579abdb5e5df3abef92c
daritos <nekttuman@gmail.com>
2023-11-30 18:13:39 +0300

    styles update

--- HiddenChess/Source/main.cpp
+++ HiddenChess/Source/main.cpp
@@ -5,12 +5,28 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-//    GameWidget w;
-//    w.show();
-    MainWindow hc(nullptr);
-    hc.show();
 
 
+
+//    GameWidget gw;
+//    gw.show();
+
+
+// Load an application style
+    QFile styleFile(":/MainWindow/style.qss");
+    styleFile.open(QFile::ReadOnly);
+    qDebug() << "Style file opened: " << styleFile.isOpen();
+
+// Apply the loaded stylesheet
+    QString style(styleFile.readAll());
+
+
+    MainWindow mw(nullptr);
+    mw.show();
+
+    app.setStyleSheet(style);
+    app.style()->unpolish(&mw);
+    app.style()->polish(&mw);
 
     return app.exec();
 }
